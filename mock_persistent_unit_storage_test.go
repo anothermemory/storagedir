74d02e17255ac0cedbef7ae326cedbbfbcf803ee
Vyacheslav Enis <venis@difane.com>
2018-01-10 01:23:13 +0200

    Update mock_persistent_unit_storage_test according to the interface change

--- mock_persistent_unit_storage_test.go
+++ mock_persistent_unit_storage_test.go
@@ -129,6 +129,29 @@ func (m *mockPersistentUnitStorage) LoadUnit(id string) (unit.Unit, error) {
 	return r0, r1
 }
 
+// MarshalJSON provides a mock function with given fields:
+func (m *mockPersistentUnitStorage) MarshalJSON() ([]byte, error) {
+	ret := m.Called()
+
+	var r0 []byte
+	if rf, ok := ret.Get(0).(func() []byte); ok {
+		r0 = rf()
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).([]byte)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func() error); ok {
+		r1 = rf()
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // Remove provides a mock function with given fields:
 func (m *mockPersistentUnitStorage) Remove() error {
 	ret := m.Called()
@@ -164,6 +187,20 @@ func (m *mockPersistentUnitStorage) SaveUnit(u unit.Unit) error {
 	var r0 error
 	if rf, ok := ret.Get(0).(func(unit.Unit) error); ok {
 		r0 = rf(u)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
+}
+
+// UnmarshalJSON provides a mock function with given fields: _a0
+func (m *mockPersistentUnitStorage) UnmarshalJSON(_a0 []byte) error {
+	ret := m.Called(_a0)
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func([]byte) error); ok {
+		r0 = rf(_a0)
 	} else {
 		r0 = ret.Error(0)
 	}
